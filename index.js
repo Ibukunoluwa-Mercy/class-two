bce5ebafec709ddab6fb3ed9b73e13345e5bafdd
Ibukunoluwa Ojedapo <ibukunojedapo2022@gmail.com>
2026-07-15 10:57:55 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express()
+const port = 3650
+const worldCup = [
+
+{
+    country: "Nigeria",
+    player: "Victor Oshimen",
+    coach: "Finidi George"
+},
+
+{
+    country: "Argentina",
+player: "Lionel Messi",
+coach: "Lionel Scaloni"
+},
+
+{
+    Country: "France",
+player: "Kylian Mbappe",
+coach: "Didier Deschamps"
+},
+{
+    Country: "Brazil",
+player: "Neymar Jr",
+coach: "Tite"
+},
+{
+    country: "Germany",
+player: "Manuel Neuer",
+coach: "Hansi Flick"
+},
+{
+    country: "Spain",
+player: "Sergio Ramos",
+coach: "Luis Enrique"
+}
+
+
+]
+
+app.get("/", (req, res) => {
+    res.send('welcome to backend class')
+    // console.log('I have accessed the path');
+    
+})
+
+app.get("/worldcup", (req, res) => {
+    res.send(worldCup)
+})
+
+app.listen(port, ()=> {
+console.log(`server running on port ${port}`);
+
+})
